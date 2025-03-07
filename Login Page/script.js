64ab2aede75fa4ea52bfa9c3c734a0e6dba2dfd0
Krish Bhargava <bhargavakrish03@gmail.com>
2025-03-07 23:25:48 +0530

    done

--- Login Page/script.js
+++ Login Page/script.js
@@ -1,94 +1,63 @@
-$(document).ready(function () {
-  $("#loginForm").submit(function (e) {
-    e.preventDefault();
-    let email = $("#email").val();
-    let password = $("#password").val();
+document.addEventListener("DOMContentLoaded", function () {
+  const loginContainer = document.getElementById("loginContainer");
+  const signupContainer = document.getElementById("signupContainer");
+  const showSignup = document.getElementById("showSignup");
+  const showLogin = document.getElementById("showLogin");
+  const githubLogin = document.getElementById("githubLogin");
+  const loginButton = document.getElementById("loginButton");
+  const googleReset = document.getElementById("googleReset");
+  const githubReset = document.getElementById("githubReset");
 
-    if (email === "admin@example.com" && password === "password123") {
-      alert("Login Successful!");
-    } else {
-      alert("Invalid Credentials");
-    }
-  });
-
-  $("#signupForm").submit(function (e) {
-    e.preventDefault();
-    let name = $("#signupName").val();
-    let email = $("#signupEmail").val();
-    let password = $("#signupPassword").val();
-
-    alert("Sign Up Successful for " + name + "!");
-  });
-
-  $("#showSignup").click(function () {
-    $("#loginContainer").addClass("hidden");
-    $("#signupContainer").removeClass("hidden");
-  });
-
-  $("#showLogin").click(function () {
-    $("#signupContainer").addClass("hidden");
-    $("#loginContainer").removeClass("hidden");
-  });
-});
-
-window.fbAsyncInit = function () {
-  FB.init({
-    appId: "YOUR_FACEBOOK_APP_ID", // Replace with your actual Facebook App ID
-    cookie: true,
-    xfbml: true,
-    version: "v12.0",
-  });
-  FB.AppEvents.logPageView();
-};
-
-(function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {
-    return;
+  if (showSignup) {
+    showSignup.addEventListener("click", function (event) {
+      event.preventDefault();
+      loginContainer.classList.add("hidden");
+      signupContainer.classList.remove("hidden");
+    });
   }
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
 
-function checkFacebookLogin() {
-  FB.getLoginStatus(function (response) {
-    if (response.status === "connected") {
-      alert("Facebook Login Successful!");
-    } else {
-      FB.login(
-        function (response) {
-          if (response.status === "connected") {
-            alert("Facebook Login Successful!");
-          } else {
-            alert("Facebook login failed.");
-          }
-        },
-        { scope: "public_profile,email" }
-      );
-    }
-  });
-}
+  if (showLogin) {
+    showLogin.addEventListener("click", function (event) {
+      event.preventDefault();
+      signupContainer.classList.add("hidden");
+      loginContainer.classList.remove("hidden");
+    });
+  }
 
-document.getElementById("fbLogin").addEventListener("click", function () {
-  checkFacebookLogin();
+  if (githubLogin) {
+    githubLogin.addEventListener("click", function () {
+      window.location.href =
+        "https://github.com/login/oauth/authorize?client_id=YOUR_GITHUB_CLIENT_ID";
+    });
+  }
+
+  if (loginButton) {
+    loginButton.addEventListener("click", function () {
+      let username = document.getElementById("username").value;
+      let password = document.getElementById("password").value;
+
+      if (username && password) {
+        alert("Login successful!");
+        // Add authentication logic here
+      } else {
+        alert("Please enter both username and password.");
+      }
+    });
+  }
+
+  window.handleCredentialResponse = function (response) {
+    console.log("Google login response:", response);
+  };
+
+  if (googleReset) {
+    googleReset.addEventListener("click", function () {
+      alert("Google password reset not implemented yet.");
+    });
+  }
+
+  if (githubReset) {
+    githubReset.addEventListener("click", function () {
+      alert("GitHub password reset not implemented yet.");
+    });
+  }
 });
-
-function handleCredentialResponse(response) {
-  console.log("Encoded JWT ID token: ", response.credential);
-  alert("Google Login Successful!");
-}
-
-window.onload = function () {
-  google.accounts.id.initialize({
-    client_id:
-      "293773847239-r0u2r64urb3flqqp42sqt83dn62brnb2.apps.googleusercontent.com",
-    callback: handleCredentialResponse,
-  });
-  google.accounts.id.renderButton(document.querySelector(".g_id_signin"), {
-    theme: "outline",
-    size: "large",
-  });
-};
